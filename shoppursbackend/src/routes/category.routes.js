f12c5fe8d361ff1f6c598d1a26338e47e46c29fa
yatin vohra <vohrayatin9@gmail.com>
2025-06-16 19:34:31 +0530

    s

--- shoppursbackend/src/routes/category.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCategoryList, getSubCategoriesByCategoryId } = require('../controllers/category.controller');
-const authMiddleware = require('../middleware/auth.middleware');
-
-router.get('/list', authMiddleware, getCategoryList);
-router.get('/:categoryId/subcategories', authMiddleware, getSubCategoriesByCategoryId);
-
-module.exports = router; 
